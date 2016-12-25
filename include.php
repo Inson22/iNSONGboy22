1351967ca60be1c3b2477dbae9ba7c11eac59a74
Inson22 <saygo.y2@gmail.com>
2016-12-25 11:46:54 +0800

    Add files via upload

---
+++ include.php
@@ -0,0 +1,59 @@
+<style type="text/css">
+.color{
+	color: rgba(0, 9, 128,);
+	font-weight: 400;
+}
+body{background-image: url(bg.jpg);
+background-size: cover;
+}
+*{
+
+	color: rgba(0, 9, 128, 0.9);
+	font-family: Calibri, sans-serif;
+	
+	font-weight: 400;
+}
+	div{
+		margin-top: 100px;
+		border: 1px solid;
+		width: 400px;
+		height: 460px;
+		background-color: rgba(59, 166, 188, .8);
+		border-radius: 10px;
+		border: none;
+	}
+
+.input{
+	padding: 15px;
+	border-radius: 5px;
+	border: none;
+	width: 200px;
+	color: #000380;
+	font-size: 17px;
+	font-weight: 200;
+}
+.select{
+	padding: 5px;
+	border-radius: 5px;
+	border: none;
+	font-size: 17px;
+	font-weight: 200;
+	width: 115px;
+
+}
+.CALCULATE{
+	border: none;
+	border-radius: 5px;
+	padding: 5px;
+	font-size: 17px;
+	font-weight: 200;
+	        background-color: rgb(235, 85, 85);
+	    color: #fff;
+	    width: 100px;
+	   
+	    transition-duration: 400ms;
+}
+.CALCULATE:hover{
+	    background-color: rgba(86, 85, 235, 0.75);
+}
+</style>
